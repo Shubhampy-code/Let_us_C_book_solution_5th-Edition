881bba1b44c8aa2d17fa7715e155d520cb559406
Shubham Shrivastava <shubham.sds@gmail.com>
2021-05-15 20:17:26 +0530

    The length & breadth of a rectangle and radius of a circle are input through the keyboard. Write a program to calculate the area & perimeter of the rectangle, and the area & circumference of the circle.

---
+++ Chapter 1- Getting Started/let_us_c_1_(H_E).py
@@ -0,0 +1,22 @@
+print("for rectangle" )
+dim1=input("Enter the length  ")
+dim1=float(dim1)
+dim2=input("Enter the breadth  ")
+dim2=float(dim2)
+Area=dim1*dim2
+print("Area of Rectangle:"+str(Area))
+print("for circle")
+dim3 = input("Enter the radius of circle")
+dim3=float(dim3)
+area = 3.1415*dim3*dim3
+circumference=2*3.1415*dim3
+print("Area of circle:"+str(area))
+print("circumference of circle:"+str(circumference))
+
+
+
+
+"""The length & breadth of a rectangle and radius of a circle are
+input through the keyboard. Write a program to calculate the
+area & perimeter of the rectangle, and the area &
+circumference of the circle"""
