c6c00e9e68aa979bb5919fa4e10e3dabff7e52dd
Shubham Shrivastava <shubham.sds@gmail.com>
2021-05-15 20:11:04 +0530

    Temperature of a city in Fahrenheit degrees is input through the keyboard. Write a program to convert this temperature into Centigrade degrees.

---
+++ Chapter 1- Getting Started/let_us_c_1_(H_D).py
@@ -0,0 +1,8 @@
+Temperature = input("Temperature of a city in Fahrenheit degrees:")
+temp = float(Temperature)
+Centigrade_degrees = (temp - 32 )*(5/9)
+print("Temperature of a city in Centigrade degrees:" + str(Centigrade_degrees))
+
+"""Temperature of a city in Fahrenheit degrees is input through
+the keyboard. Write a program to convert this temperature
+into Centigrade degrees. """
