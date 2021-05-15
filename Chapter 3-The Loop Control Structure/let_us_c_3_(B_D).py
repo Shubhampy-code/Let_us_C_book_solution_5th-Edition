97ab3b3d7e4c6bb57ac62c920386f14ceadddeaa
Shubham Shrivastava <shubham.sds@gmail.com>
2021-05-15 22:38:02 +0530

    Write a program to print all the ASCII values and their equivalent characters using a while loop. The ASCII values vary from 0 to 255.

---
+++ Chapter 3-The Loop Control Structure/let_us_c_3_(B_D).py
@@ -0,0 +1,8 @@
+"""Write a program to print all the ASCII values and their
+equivalent characters using a while loop. The ASCII values
+vary from 0 to 255."""
+number = 0
+while (number <= 255):
+    print(   str(number ) +" " +chr (number) )
+    number = number + 1
+
