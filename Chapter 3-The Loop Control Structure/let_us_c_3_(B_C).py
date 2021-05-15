97124b403231ffa6ee8572d4b92e7e9d41455dc9
Shubham Shrivastava <shubham.sds@gmail.com>
2021-05-15 22:37:03 +0530

    Two numbers are entered through the keyboard. Write a program to find the value of one number raised to the power of another.

---
+++ Chapter 3-The Loop Control Structure/let_us_c_3_(B_C).py
@@ -0,0 +1,21 @@
+"""Two numbers are entered through the keyboard. Write a
+program to find the value of one number raised to the power
+of another."""
+
+"""a = float(input("Enter number : "))
+b = int(input("Enter number : "))
+value = 1
+i=1 
+while i <=b :
+    value = value*a 
+    
+    i=i+1
+print(value)     
+"""
+a = float(input("Write the first number : "))
+b = int(input("Write the second number : "))
+value = 1
+while b > 0 : 
+    value = a * value
+    b = b-1
+print(value)
