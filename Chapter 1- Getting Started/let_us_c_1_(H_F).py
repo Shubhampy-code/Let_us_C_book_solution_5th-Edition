7ab421ee718ac21903a5a20b53b2422d0f3b0e73
Shubham Shrivastava <shubham.sds@gmail.com>
2021-05-15 20:19:11 +0530

    Two numbers are input through the keyboard into two locations C and D. Write a program to interchange the contents of C and D.

---
+++ Chapter 1- Getting Started/let_us_c_1_(H_F).py
@@ -0,0 +1,11 @@
+"""Two numbers are input through the keyboard into two
+locations C and D. Write a program to interchange the
+contents of C and D. """
+
+C=input("Enter the number for C:")
+D=input("Enter the number for D:")
+A=C
+C=D
+D=A
+print("C:"+C)
+print("D:"+D)
