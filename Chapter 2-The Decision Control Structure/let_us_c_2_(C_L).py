6d53ae6f94bff549edf75560e23f7fd5ee0e3b91
Shubham Shrivastava <shubham.sds@gmail.com>
2021-06-06 17:14:37 +0530

    Update let_us_c_2_(C_L).py

--- Chapter 2-The Decision Control Structure/let_us_c_2_(C_L).py
+++ Chapter 2-The Decision Control Structure/let_us_c_2_(C_L).py
@@ -1,13 +1,13 @@
 """Given a point (x, y), write a program to find out if it lies on the
 x-axis, y-axis or at the origin, viz. (0, 0). """
-x,y = input("Enter the point").split()
+x,y = input("Enter the point : ").split()
 x = float(x)
 y = float(y)
-if x == 0:
+if ( x == 0) and (y == 0):
+    print("point lies on origin")      
+elif x == 0:
     print("Point lies on y-axis")
 elif y == 0:
     print("point lies on x-axis")
-elif ( x == 0) and (y == 0):
-    print("point lies on origin")      
 else:
-    print("Given point is not lies on origin and x,y axis")      
+    print("Given point is not lies on origin and x,y axis")  
